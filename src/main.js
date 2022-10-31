a77604db86d0ad9465be3f70f5fd3ed21fcaa41f
Pedro Fonseca Esteves <muradean@gmail.com>
2022-10-31 22:30:46 +0000

    successfully added vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
+import 'vuetify/styles' // Global CSS has to be imported
 import { createApp } from 'vue'
-import './style.css'
+import { createVuetify } from 'vuetify'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//this two lines are need for the vuetiy to take place
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+
+//need to include the components and the directives in the vuetify
+const vuetify = createVuetify({
+  ssr: true,
+  components,
+  directives
+})
+
+app.use(vuetify)
+app.mount('#app')
