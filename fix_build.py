8991f0bba61f6f7e373279e26441165caafed0e9
Pedro Fonseca Esteves <muradean@gmail.com>
2022-12-04 17:34:40 +0000

    new build

--- fix_build.py
+++ fix_build.py
@@ -1,1 +1,16 @@
-print("macaco")
+index_path = "/home/pedro/Desktop/darealesteves/dist/index.html"
+
+print("Fixing " + index_path)
+
+content = None
+with open(index_path, "r") as f:
+    content = f.read() 
+    content = content.replace('src="','src=".') 
+    content = content.replace('href="','href=".')
+    f.close()
+
+print(content)
+
+f = open(index_path, "w")
+f.write(content)
+f.close()
