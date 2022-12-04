7bed0eccd19c4eef7a70fced5a365cab4d0549d6
Pedro Fonseca Esteves <muradean@gmail.com>
2022-12-04 16:55:17 +0000

    new build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/ antes tinha base: '/online-cv/' 
 export default defineConfig({
-  base: '/',
+  base: '/darealesteves/',
   plugins: [vue()]
 })
