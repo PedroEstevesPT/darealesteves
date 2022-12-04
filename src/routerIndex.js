2063d5bcee930894ff77822b0660559a2cae1df2
Pedro Fonseca Esteves <muradean@gmail.com>
2022-12-04 17:51:30 +0000

    new build

--- src/routerIndex.js
+++ src/routerIndex.js
@@ -5,8 +5,13 @@ import Atelier from "./views/Atelier.vue";
 
 import Fassbinder from "./components/articles/cinema/Fassbinder.vue";
 
+var repo = 'darealesteves'
 const routes = [
+  { path: '/darealesteves', redirect: '/professional' },
   { path: '/', redirect: '/professional' },
+//  { path: repo + '/professional', redirect: '/professional' },
+ // { path: repo + '/blog', redirect: '/professional' },
+//  { path: repo + '/blog/cinema/Fassbinder', redirect: "/blog/cinema/Fassbinder" },
   {
     path: "/professional",
     name: "Professional",
