e3425122762653e4bc04917298fdae77ccc158ca
Pedro Fonseca Esteves <muradean@gmail.com>
2023-02-23 19:39:38 +0000

    changed cv link

--- src/components/DownloadableLink.vue
+++ src/components/DownloadableLink.vue
@@ -7,8 +7,8 @@
     name: 'DownloadableLink',
     props: ["text"],
     data() {
-        return {
-            pdfUrl: 'https://www.dropbox.com/s/se7dciriy21cpwz/Pedro_Esteves_CV.pdf?dl=1'
+        return { 
+            pdfUrl: 'https://www.dropbox.com/s/qqbp9ot4ofv4v5r/CV-202.pdf?dl=1'
         }
     }
     };
