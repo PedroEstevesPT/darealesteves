d2f2d4d1adb15518eeb2c3484a6cf6c0ab6e27a2
Pedro Fonseca Esteves <muradean@gmail.com>
2023-04-25 03:02:53 +0100

    added queries to dynamics ce web api page

--- src/components/articles/fitness/RunningShoes.vue
+++ src/components/articles/fitness/RunningShoes.vue
@@ -4,6 +4,8 @@
     <h1 class="blue-text">
     Os meus sapatos de corrida
     </h1>
+
+    <BreadCrumbs :items="breadcrumbs"/>
     
     <br>
 
@@ -28,28 +30,35 @@ import BreadCrumbs from   '../../../components/BreadCrumbs.vue';
 
 export default {
   name: 'ETFs',
-  components: {ArticleFigure},
+  components: {ArticleFigure,BreadCrumbs},
   computed: { 
   },
   data: () => ({
+    breadcrumbs: [
+      {"title": {"en":'Blog' ,  "pt": "Blog"}, "path":"/blog"},
+      {"title": {"en":'Fitness', "pt": "Fitness"}, "path":"/blog/fitness"},
+      {"title": {"en":'My Running Shoes', 
+      "pt": "Os meus sapatos de corrida"}, "path":"/blog/fitness/runningshoes"}
+    ],
     kiprun: { 
         "img": "https://res.cloudinary.com/dho8ay2wz/image/upload/v1682292071/pedrofortunatoesteves-site/blog/fitness/kiprun_kd_plus_rfltuc.png",
         "description": {
-          "pt": "Os primeiros sapatos desta lista são os Kiprun Kd Plus. Gosto muito deles e são bastante confortáveis, quer no calcanhar como de lado, tenho-os pelo menos desde 2021."
+          "pt": "Os primeiros sapatos desta lista são os Kiprun Kd Plus. Gosto muito deles e são bastante confortáveis, quer no calcanhar como de lado, tenho-os pelo menos desde 2020.\
+          Eram o meu calçado no photoshoot do Trash4Goods em 2020. Atualmente estão reformados mas ainda saio com eles à rua uma ou duas vezes."
         }
     },
     gelZirus: { 
         "img": "https://res.cloudinary.com/dho8ay2wz/image/upload/v1682292059/pedrofortunatoesteves-site/blog/fitness/gel-ziruss4_rb3rbp.png",
         "description": {
-          "pt": "Os Gel-Ziruss 4 da Aisics foram uns sapatos que comprei depois dos Kiprun Kd Plus. Usava-os sobretudo para as minhas corridas dos 10 km. \
-          Durante muito tempo foram os meus sapatos de eleição, mas em certas corridas causavam-me feridas nos calcanhares, portanto não são de todos os meus sapatos favoritos e encontram-se reformados."
+          "pt": "Os Gel-Ziruss 4 da Aisics foram uns sapatos que comprei depois dos Kiprun Kd Plus. Usava-os sobretudo para as minhas corridas dos 10 km entre 2021 e 2022. \
+          Durante muito tempo foram os meus sapatos de eleição, mas em certas corridas causavam-me feridas nos calcanhares, portanto não são de todo os meus sapatos favoritos e encontram-se reformados (os quilómetros em cima e os arranhões de umas gatas não perdoam :) )."
         }
     },
     gelNimbus: { 
         "img": "https://res.cloudinary.com/dho8ay2wz/image/upload/v1682292053/pedrofortunatoesteves-site/blog/fitness/gel-nimbus_fz0r0j.png",
         "description": {
-          "pt": "Comprei os Gel-Nimbus 24 da Aisics no verão de 2022. Comparando com os  Gel-Ziruss 4 e Kiprun Kd Plus os Nimbus são consideravelmente mais caros mas são de longe os melhores.\
-          Para além de serem super confortáveis, acho que me ajudam na corrida como nenhum outro sapato me ajuda \
+          "pt": "Comprei os Gel-Nimbus 24 da Aisics (A.K.A. Neons Fluorescentes dos Tunings ) no verão de 2022. Comparando com os  Gel-Ziruss 4 e Kiprun Kd Plus os Nimbus são consideravelmente mais caros mas são de longe os melhores.\
+          Para além de serem super confortáveis, lembro-me de quando os calçei e fui correr com eles pela primeira vez, parecia que voava, sentia-me tão leve, à semelhança de quando contraímos os braços depois fazermos alguns biceps curls e largarmos os pesos.\
           Lembro-me que tinha corrido 15 km e ficado a sangrar dos calcanhares com os Gel-Ziruss 4 e passados 1-2 dias fui comprar os Gel-Nimbus 24 e voltei a correr a distância mais longa \
           que alguma vez corri na vida: 21 km. No entanto, habitualmente uso-os nas minhas corridas de 10km."
         }
--- src/components/articles/tech/DynamicsCEWebAPIQueries.vue
+++ src/components/articles/tech/DynamicsCEWebAPIQueries.vue
@@ -5,6 +5,39 @@
 
       <BreadCrumbs  :items="breadcrumbs" />
 
+      <br>
+      <h2 class="blue-text pe-text">Web API Query Index</h2>
+      <li class="pe-text underline" @click="scrollToElement('ShowSolutionLayers')"> Show Solution Layers Of A Component</li>
+      <li class="pe-text underline" @click="scrollToElement('ListComponents')"> List Solution Components</li>
+      <li class="pe-text underline" @click="scrollToElement('AddComponents')"> Add Component To A Solution </li>
+      <li class="pe-text underline" @click="scrollToElement('RemoveComponents')"> Remove Component From A Solution</li>
+      <br>
+
+      <h2 id="ListComponents" class="pe-text blue-text"> List Solution Components</h2>
+      <p> 
+        <b>Query: </b> org_url/api/data/v9.0/solutioncomponents?$filter=_solutionid_value eq &ltsolution_id&gt  
+        <br><br>
+        This query is useful because not all components are visible through the Legacy and the make.powerapps UI. <br>
+        Furthermore this query also shows the componenttype of each component which is an important attribute required by other queries.
+      </p>
+      <ArticleFigure :figure=listComponents />
+
+
+      <h2 id="ShowSolutionLayers" class="pe-text blue-text"> Show Solution Layers of A Component </h2>
+      <p> 
+        <b>Query: </b> org_url/api/data/v9.2/RetrieveSolutionMetadataForComponent(SolutionComponentName='SLAItem',ColumnNames=['slaitemid'],ColumnValues=[&ltslaitemid&gt])
+        <br><br>
+        For some components (which are not visible through the UI) an alternative to check their solution layers is through the Web API as it can be seen below.
+      </p>
+      <ArticleFigure :figure=showSolutionLayers />
+
+
+
+      <h2 id="ListComponents" class="pe-text blue-text"> Add Component To A Solution </h2>
+
+      <h2 id="ListComponents" class="pe-text blue-text"> Remove Component From A Solution </h2>
+
+
     </div>
   </v-container>
 </template>
@@ -23,38 +56,24 @@ export default {
       {"title": {"en": "Tech", "pt": "Tech"},    "path":"/blog/tech"},
       {"title": {"en": "Dynamics CE Web API Queries", "pt": "Dynamics CE Web API Queries"}, "path":"/blog/tech/DynamicsCEWebAPIQueries"},
     ],
-    example1: { 
-        "img": "https://res.cloudinary.com/dho8ay2wz/image/upload/v1677381520/pedrofortunatoesteves-site/blog/museums/IMG_0377_zwbjdu.jpg",
+    showSolutionLayers: { 
+        "img": "https://res.cloudinary.com/dho8ay2wz/image/upload/v1682385892/pedrofortunatoesteves-site/blog/tech/webAPIQueries/Screenshot_from_2023-04-25_02-24-10_h8519y.png",
         "description": {
-          "pt": "Bustos de Nefertiti",
-          "en": "The first room (3rd floor)."
+          "pt": "Lista as componentes que existem dentro de uma solução",
+          "en": "List the components inside a solution."
         }
     },
-    example2: { 
-        "img": "https://res.cloudinary.com/dho8ay2wz/image/upload/v1677381520/pedrofortunatoesteves-site/blog/museums/IMG_0379_npnwvx.jpg",
+    listComponents: { 
+        "img": "https://res.cloudinary.com/dho8ay2wz/image/upload/v1682385827/pedrofortunatoesteves-site/blog/tech/webAPIQueries/listComponentsOfSolution_shwo4f.png",
         "description": {
-          "pt": "A primeira sala (terceiro piso).",
-          "en": "The first room (3rd floor)."
-        }
-    },
-    example3: { 
-        "img": "https://res.cloudinary.com/dho8ay2wz/image/upload/v1677381519/pedrofortunatoesteves-site/blog/museums/IMG_0382_xdkpbs.jpg",
-        "description": {
-          "pt": "A primeira sala (terceiro piso).",
-          "en": "The first room (3rd floor)."
-        }
-    },
-    example4: { 
-        "img": "https://res.cloudinary.com/dho8ay2wz/image/upload/v1677381520/pedrofortunatoesteves-site/blog/museums/IMG_0384_df6rfi.jpg",
-        "description": {
-          "pt": "A primeira sala (terceiro piso).",
-          "en": "The first room (3rd floor)."
+          "pt": "Lista as componentes que existem dentro de uma solução-",
+          "en": "List the components that exist inside a solution."
         }
     }
 
-
     
-  })
+  }),
+  
 }
 </script>
 <style lang="scss" scoped>
