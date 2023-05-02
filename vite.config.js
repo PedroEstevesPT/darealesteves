d67fc907498727daa6e9c7bbd96e2a9e08ce6e76
Pedro Fonseca Esteves <muradean@gmail.com>
2023-05-02 17:50:48 +0100

    new deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/ antes tinha base: '/online-cv/' 
+// https://vitejs.dev/config/ antes tinha base: '/online-cv/'
+//For github pages deployment I need base to be like this: '/darealesteves/',   
 export default defineConfig({
-  base: '/darealesteves/',
+  base: '/',
   plugins: [vue()]
 })
